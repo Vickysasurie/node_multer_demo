70bdb03f9cd30899e80b2c45d016b49f93fbf1f7
Vickysasurie <vigneshwaransasurie@gmail.com>
2019-04-04 11:05:40 +0530

    change all

---
+++ api/router/pdfMake.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/pdf', (req,res,next)=>{
+    console.log('Get routes is called');
+
+});
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 //require the express router
 var router = express.Router();
 const app = express();
-//const path = require('path');
+const path = require('path');
 const bodyparser = require('body-parser');
 //var db = require('./database');
 var infoRouter = require('./api/router/student_info_router');
@@ -45,7 +45,8 @@ app.get('/', (req,res) => {
     //for send file to view
     //res.sendFile("Welcome to task node project");
     //for send text
-    res.json({"message":"Welcome to task node project"});
+    // res.json({"message":"Welcome to task node project"});
+    res.send('Hello all! Welcome to test app node js');
 });
 
 app.use(cors()); 
@@ -66,7 +67,39 @@ app.post('/', function (req, res, next) {
        return res.send("Upload Completed for "+path); 
  });     
 })
+var qr = require('qr-image');
+app.post('/qr', function(req,res,next){
+  var arr=[]
+  //var arr = req.query.value
+console.log(req.body.value);
+req.body.value.forEach((a)=>{
+  
+ 
+  arr.push( qr.image(a));
 
+})
+  
+  res.setHeader('Content-type', 'application/octet-stream');  //sent qr image to client side
+  res.set("Content-Dispostion", "attachment;filename = google.png");
+  res.writeHead(200, {'Content-Type': 'image/png'}); //for image display as qr
+  img.pipe(res);
+  
+  });
+  app.use(express.static(path.join(__dirname, 'image')));
+  var storage = multer.diskStorage({
+    // destination
+    destination: function (req, file, cb) {
+      cb(null, './image/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  var upload = multer({ storage: storage });
 
+  app.post("/upload", upload.array("uploads[]", 12), function (req, res) {
+    console.log('files', req.files);
+    res.send(req.files);
+  });
 
 module.exports = app;
